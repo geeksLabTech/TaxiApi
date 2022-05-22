7ed2cd23b8558f3a3b285d8a82f5e13b8fc09588
Daniel <danielcardenascabrera2016@gmail.com>
2022-05-22 19:03:08 -0400

    Basic_testing_implementation_wip

---
+++ tests/test_crud_passenger.py
@@ -0,0 +1,40 @@
+from test_sql_app import client
+
+def test_create_passenger():
+    response = client.post("/passenger/", json={"name": "John Doe", "phone_number": 55193109, "password": "12345"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "John Doe"
+    assert data["phone_number"] == 55193109
+    assert data["password"] == "12345"
+    
+
+def test_get_passenger():
+    response = client.get("/passenger/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "John Doe"
+    assert data["phone_number"] == 55193109
+    assert data["password"] == "12345"
+
+
+def test_get_passenger_by_phone_number():
+    response = client.get("/passenger/phone_number/55193109")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "John Doe"
+    assert data["phone_number"] == 55193109
+    assert data["password"] == "12345"
+
+
+def test_get_all_passengers():
+    response = client.get("/passenger/")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 1
+    assert data[0]["name"] == "John Doe"
+    assert data[0]["phone_number"] == 55193109
+    assert data[0]["password"] == "12345"
+
+
+
---
+++ tests/test_sql_app.py
@@ -0,0 +1,33 @@
+import imp
+#from test_main import client
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .database import Base
+from fastapi.testclient import TestClient
+from .main import app
+from .dependencies import get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
