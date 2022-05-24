49fe9cb8093850fddd001fe0953fac02a8a48d13
Daniel <danielcardenascabrera2016@gmail.com>
2022-05-24 01:23:06 -0400

    Improve_test_impl_not_working

--- tests/test_crud_passenger.py
+++ tests/test_crud_passenger.py
@@ -1,6 +1,7 @@
-from .test_sql_app import client
+from .test_sql_app import client, session
+from .test_data.test_passenger_data import PASSENGER_ONE, PASSENGER_TWO, PASSENGER_THREE, PASSENGER_FOUR
 
-def test_create_passenger():
+def test_create_passenger(client):
     response = client.post("/passenger/", json={"id": 1, "name": "John Doe", "phone_number": 55193109, "password": "12345"})
     assert response.status_code == 200
     data = response.json()
@@ -9,7 +10,7 @@ def test_create_passenger():
     assert data["password"] == "12345"
     
 
-def test_get_passenger():
+def test_get_passenger(client):
     response = client.get("/passenger/1")
     assert response.status_code == 200
     data = response.json()
@@ -18,7 +19,7 @@ def test_get_passenger():
     assert data["password"] == "12345"
 
 
-def test_get_passenger_by_phone_number():
+def test_get_passenger_by_phone_number(client):
     response = client.get("/passenger/phone_number/55193109")
     assert response.status_code == 200
     data = response.json()
@@ -27,14 +28,30 @@ def test_get_passenger_by_phone_number():
     assert data["password"] == "12345"
 
 
-#def test_get_all_passengers():
-#    response = client.get("/passenger/")
-#    assert response.status_code == 200
-#    data = response.json()
-#   assert len(data) == 1
-#    assert data[0]["name"] == "John Doe"
-#    assert data[0]["phone_number"] == "55193109"
-#    assert data[0]["password"] == "12345"
+def test_get_all_passengers(client):
+    response = client.get("/passenger/")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 4
+    assert data[0]["name"] == PASSENGER_ONE["name"]
+    assert data[0]["phone_number"] == PASSENGER_TWO["phone_number"]
+    assert data[0]["password"] == PASSENGER_THREE["password"]
+    assert data[1]["name"] == PASSENGER_TWO["name"]
+    assert data[1]["phone_number"] == PASSENGER_TWO["phone_number"]
+    assert data[1]["password"] == PASSENGER_TWO["password"]
+    assert data[2]["name"] == PASSENGER_THREE["name"]
+    assert data[2]["phone_number"] == PASSENGER_THREE["phone_number"]
+    assert data[2]["password"] == PASSENGER_THREE["password"]
+    assert data[3]["name"] == PASSENGER_FOUR["name"]
+    assert data[3]["phone_number"] == PASSENGER_FOUR["phone_number"]
+    assert data[3]["password"] == PASSENGER_FOUR["password"]
+
+
+def test_delete_passenger(client):
+    response = client.delete("/passenger/1")
+    assert response.status_code == 200
+    get_response = client.get("/passenger/1")
+    assert get_response.status_code == 404
 
 
 
--- tests/test_sql_app.py
+++ tests/test_sql_app.py
@@ -1,12 +1,24 @@
 
+from typing import Iterable
+import pytest
+
 #from test_main import client
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from .test_data.test_driver_data import DRIVERS_DB
+from .test_data.test_driver_vechicle_data import DRIVERS_VEHICLES_DB
+from .test_data.test_famousplaces_data import FAMOUS_PLACES_DB
+
+from .test_data.test_passenger_data import PASSENGERS_DB
+from .test_data.test_place_data import PLACES_DB
+from .test_data.test_trip_data import TRIPS_DB
+from .test_data.test_vehicle_data import VECHICLES_DB
 
 from ..database import Base
 from fastapi.testclient import TestClient
 from ..main import app
 from ..dependencies import get_db
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -16,18 +28,52 @@ engine = create_engine(
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-Base.metadata.create_all(bind=engine)
+def populate_test_db_with_fake_data(db):
+    data_to_add = []
+    for passenger in PASSENGERS_DB:
+        data_to_add.append(passenger)
+    for driver in DRIVERS_DB:
+        data_to_add.append(driver)
+    for vehicle in VECHICLES_DB:
+        data_to_add.append(vehicle)
+    for place in PLACES_DB:
+        data_to_add.append(place)
+    for trip in TRIPS_DB:
+        data_to_add.append(trip)
+    for drivervehicle in DRIVERS_VEHICLES_DB:
+        data_to_add.append(drivervehicle)
+    for famous in FAMOUS_PLACES_DB:
+        data_to_add.append(famous)
+        
+    db.add_all(data_to_add)
+    db.commit()
 
 
-def override_get_db():
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
+    populate_test_db_with_fake_data(db)
+
     try:
         yield db
     finally:
         db.close()
 
 
+@pytest.fixture()
+def client(session) -> Iterable[TestClient]:
 
-app.dependency_overrides[get_db] = override_get_db
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
 
-client = TestClient(app)
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
+
+
