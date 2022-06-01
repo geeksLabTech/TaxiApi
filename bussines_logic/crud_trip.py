b3b5e8c7fffa2dfd26a7bf20699487234888d292
JavierOramas <javiale2000@gmail.com>
2022-06-01 16:28:05 -0400

    apagon safety

--- bussines_logic/crud_trip.py
+++ bussines_logic/crud_trip.py
@@ -1,44 +1,47 @@
+from turtle import distance
 from sqlalchemy.orm import Session
 import models
-from models import table_trip
+from models import TripDB
 from routers import trip_route
 from schemas import DriverVehicle, Trip
 from schemas import Passenger
 
 
-def get_trip(driver_id: int, pasenger_id: int, vehicle_id: int, db: Session):
-    return db.query(table_trip).filter(table_trip.driver_id == driver_id, table_trip.pasenger_id == pasenger_id, table_trip.vehicle_id == vehicle_id).first()
+def get_trip(driver_id: int, passenger_id: int, vehicle_id: int, db: Session):
+    return db.query(TripDB).filter(TripDB.driver_id == driver_id, TripDB.passenger_id == passenger_id, TripDB.vehicle_id == vehicle_id).first()
 
 
 def get_all_trips(db: Session):
-    return db.query(table_trip).all()
+    return db.query(TripDB).all()
 
 
 def create_trip(trip: Trip, db: Session):
-    driver = db.query(models.DriverDB).filter(models.DriverDB.id == trip.driver_id).first()
-    vehicle = db.query(models.VehicleDB).filter(models.VehicleDB.id == trip.vehicle_id).first()
-    passanger = db.query(models.PassengerDB).filter(models.PassengerDB.id == trip.passnger_id).first()
+    db_trip = TripDB(
+        date=trip.date, time=trip.time, price=trip.price, distance=trip.distance, origin=trip.origin, destination=trip.destination,
+        status=trip.status, driver_id=trip.driver_id, passenger_id=trip.passenger_id, vehicle_id=trip.vehicle_id)
+
     try:
-        driver.vehicles.append(vehicle)
-        vehicle.passanger.append(passanger)
+        db.add(db_trip)
         db.commit()
-        return trip
+        db.refresh(db_trip)
+        return db_trip
+
     except:
         return "Somethig went wrong"
 
 
-# def update_trip(db: Session, driver_id: int, pasenger_id: int, vehicle_id: int, trip: table_trip):
-#     db_trip = db.query(table_trip).filter(table_trip.driver_id == driver_id,
-#                                       table_trip.pasenger_id == pasenger_id, table_trip.vehicle_id == vehicle_id).first()
+# def update_trip(db: Session, driver_id: int, passenger_id: int, vehicle_id: int, trip: TripDB):
+#     db_trip = db.query(TripDB).filter(TripDB.driver_id == driver_id,
+#                                       TripDB.passenger_id == passenger_id, TripDB.vehicle_id == vehicle_id).first()
 #     db_trip.driver_id = trip.driver_id
-#     db_trip.pasenger_id = trip.pasenger_id
+#     db_trip.passenger_id = trip.passenger_id
 #     db_trip.vehicle_id = trip.vehicle_id
 #     db.commit()
 
 
-def delete_trip(driver_id: int, pasenger_id: int, vehicle_id: int, db: Session):
-    db_trip = db.query(table_trip).filter(table_trip.driver_id == driver_id,
-                                      table_trip.pasenger_id == pasenger_id, table_trip.vehicle_id == vehicle_id).first()
+def delete_trip(driver_id: int, passenger_id: int, vehicle_id: int, db: Session):
+    db_trip = db.query(TripDB).filter(TripDB.driver_id == driver_id,
+                                      TripDB.passenger_id == passenger_id, TripDB.vehicle_id == vehicle_id).first()
     db.delete(db_trip)
     db.commit()
     return db_trip
